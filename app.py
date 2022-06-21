9d507837dc0d6bde823ea3f3a917283df9c22d03
Kevin Howlett <54913419+Kevin-Howlett@users.noreply.github.com>
2022-06-21 14:09:03 -0400

    Debugging NA col check.

--- app.py
+++ app.py
@@ -351,6 +351,21 @@ def main():
                         income, parent_edu, sap)
 
         munged_df = prepare_full_year(retention)
+
+        # Check for Missing Values
+        na_df = pd.DataFrame({'Feature': munged_df.columns.tolist(), 'Prop_NA':(munged_df.isna().sum()/munged_df.shape[0])}).reset_index(drop=True)
+
+        # Check for fully NA columns
+        if (na_df['Prop_NA'] == 1).any():
+            fully_na_features = na_df.loc[na_df['Prop_NA'] == 1]
+            st.write('### WARNING: all values are missing for the following columns:')
+            st.write(fully_na_features.rename(columns = {'Prop_NA': 'Percent_Missing'}))
+
+        # Warn user about Features missing more than 50%
+        elif (na_df['Prop_NA'] >= 0.5).any():
+            half_na_features = na_df.loc[na_df['Prop_NA'] >= 0.5].sort_values(by = 'Prop_NA', ascending=False)
+            st.write('### WARNING: at least 50%\ of values are missing for the following columns:')
+            st.write(half_na_features.rename(columns = {'Prop_NA': 'Percent_Missing'}))
         
         # Generate and store predictions
         prediction_df = output_preds(munged_df,
