e9e2858002b637f25eb3afc915d96f1a99cedc48
palazzojulian <50282600+palazzojulian@users.noreply.github.com>
2022-06-21 19:20:16 -0700

    Debugging

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ def main():
 
         st.write("FILE_NAME:",file_name)
 
-        MyVars[f"{file_name}"] = load_data(uploaded_file)
+        MyVars[file_name] = load_data(uploaded_file)
 
-        exec(f"{file_name} = MyVars[{file_name}]")
+        exec(f"{file_name} = MyVars[file_name]")
         # exec(f"{file_name} = load_data({uploaded_file})")
         # file_str_name = re.sub("_", " ", file_str_name).capitalize() # replace _ with " " and capitalize to match cols_needed dict keys
         files_read_in[file_str_name] = file_name.columns
@@ -111,8 +111,8 @@ def main():
         # if "retention" in file_name:
         #     retention = load_data(uploaded_file)
         #     files_read_in
-    if retention:
-        st.write("SHOWING THE 'RETENTION' VARIABLE:", retention)
+    # if retention:
+        # st.write("SHOWING THE 'RETENTION' VARIABLE:", retention)
 
     # INDIVIDUAL FILE UPLOAD
 
