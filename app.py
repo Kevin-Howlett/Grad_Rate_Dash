6a214df90d884458533ad424f2c83f9742829dd5
palazzojulian <50282600+palazzojulian@users.noreply.github.com>
2022-06-21 19:16:03 -0700

    Debugging

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ def main():
 
         st.write("FILE_NAME:",file_name)
 
-        globals()[f"{file_name}"] = load_data(uploaded_file)
+        vars()[f"{file_name}"] = load_data(uploaded_file)
 
-        exec(f"{file_name} = globals()[{file_name}]")
+        exec(f"{file_name} = vars()[{file_name}]")
         # exec(f"{file_name} = load_data({uploaded_file})")
         # file_str_name = re.sub("_", " ", file_str_name).capitalize() # replace _ with " " and capitalize to match cols_needed dict keys
         files_read_in[file_str_name] = file_name.columns
